5f8664ebf81528acd4b5a4bfd8139a60c854237a
emad-ali-nv22175 <149559151+emad-ali-nv22175@users.noreply.github.com>
2024-03-22 12:44:26 +0000

    vidoe 14

--- Server.js
+++ Server.js
@@ -4,6 +4,9 @@ import dotenv from "dotenv";
 
 import connectDB from "./config/db";
 
+// Routes
+import userRoutes from './routes/userRoutes';
+
 dotenv.config();
 connectDB();
 const app =express ();
@@ -12,6 +15,8 @@ app.use(express.json())
 app.get("/", (req, res) => {
     res.send("Server is running...");
   });
+
+  app.use('/api/users', userRoutes);
 
   const PORT = process.env.PORT || 5001;
 

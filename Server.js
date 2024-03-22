5343d9780331355c7c78609a824c7edca1bdfd8c
emad-ali-nv22175 <149559151+emad-ali-nv22175@users.noreply.github.com>
2024-03-22 10:22:32 +0000

    13 ..

--- Server.js
+++ Server.js
@@ -2,6 +2,8 @@ import express from "express";
 
 import dotenv from "dotenv";
 
+import connectDB from "./config/db";
+
 dotenv.config();
 connectDB();
 const app =express ();
@@ -11,6 +13,6 @@ app.get("/", (req, res) => {
     res.send("Server is running...");
   });
 
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 5001;
 
-  app.listen(5000, () => console.log ("server is running on port ${PORT}"));
+  app.listen(PORT, () => console.log (`server is running on port ${PORT}`));
